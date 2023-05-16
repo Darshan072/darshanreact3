0e94dac72b327334d05273db21b8527c67f4ee52
Darshan Ghetiya <30925505+Darshan072@users.noreply.github.com>
2023-05-16 19:31:09 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
 
+import {  useNavigate } from 'react-router-dom';
+
+const navigate = useNavigate();
+
+const handleLogin = () => {
+  navigate('/darshan');
+};
+
 function App() {
-  return <h1 className="App">Hello From BnD!</h1>;
+  return <h1 className="App" onClick={() => {
+    handleLogin();
+  }} >Hello From BnD!</h1>;
 }
 
 export default App;
